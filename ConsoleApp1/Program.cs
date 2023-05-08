ed04a44de4ed860f24d6969fcabe35a9a7e11ed0
Sergey Skuridin <skuridin@gmail.com>
2023-05-08 23:03:36 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привет Мир");
+            Console.ReadKey();
+            // Основные типы int | float | char| string | bool
+            float f = 5.7f;
+            int age = 27;
+            char c = ' ';
+            string str = "Привет, как дела";
+            
+        }
+    }
+}
