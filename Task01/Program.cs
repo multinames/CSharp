b4a8dcae99fa96e5f31f197e96428a1be30ec9e4
Sergey Skuridin <skuridin@gmail.com>
2023-05-17 23:10:48 +0300

    add new folder task01-03 + new code

---
+++ Task01/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine("Квадрат числа = " + (number*number));
+
---
+++ Task03/Program.cs
@@ -0,0 +1,15 @@
+﻿string[] days = new string[7] { "понедельник", "вторник","среда","четверг","пятница","суббота","воскресенье"};
+Console.WriteLine("Введите число дня недели");
+int number = int.Parse(Console.ReadLine());
+if (number > 7) {
+    Console.WriteLine("Не верное число"); } 
+else if (number < 1) {
+    Console.WriteLine("Не верное число"); } 
+
+else {
+    Console.WriteLine("День недели - " + days[number-1]);
+}
+
+
+
+
