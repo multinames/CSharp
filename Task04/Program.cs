b1080d64d9464e0c4bd83b52f7ac085e1f786865
Sergey Skuridin <skuridin@gmail.com>
2023-05-17 23:49:28 +0300

    add new folder task04-05 + new code

---
+++ Task04/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите число");
+
+int numberA = int.Parse(Console.ReadLine());
+int numberB = numberA*-1;
+// int i  = numberB;
+
+if (numberA > 0 ) {
+Console.WriteLine("Все целые числа:");
+for (int i  = numberB;i < numberA+1; i++) {
+
+    Console.WriteLine(i);
+}
+ }
+else {
+Console.WriteLine("Все целые числа:");
+for (int i  = numberA;i < numberB+1; i++) {
+
+    Console.WriteLine(i);
+    }
+}
+
+    
---
+++ Task05/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число");
+string numberA = Console.ReadLine();
+int len = numberA.Length-1;
+if (len > 0) {
+Console.WriteLine(numberA.Remove(0, len));
+}
+else {
+Console.WriteLine(numberA);
+}
+
+
+
+
