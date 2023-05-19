090eb7fcf9067647135566a35767b80955603897
Sergey Skuridin <skuridin@gmail.com>
2023-05-19 11:58:14 +0300

    rename folders

---
+++ Task09/Program.cs
@@ -0,0 +1,13 @@
+﻿// Проврка является ли число квадратом
+
+Console.WriteLine("Введите два числа");
+int numberA = int.Parse(Console.ReadLine());
+int numberB = int.Parse(Console.ReadLine());
+    if ((numberB*numberB) == numberA){
+    Console.WriteLine(numberA +" Является квадратом " + numberB);
+    }
+    else {
+    Console.WriteLine(numberA +" Не является квадратом " + numberB);
+    }
+
+
