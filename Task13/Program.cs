3fb5f76c19ca353ff91f6c45e303f0d5a568b3b3
cepera <s.skuridin@corp.rtln.ru>
2023-05-24 12:32:34 +0300

    add news task13

---
+++ Task13/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Введите целое число");
+
+string str = Console.ReadLine();
+
+// Проверка на число
+if ((int.TryParse(str, out int num)) == false) {
+  Console.WriteLine("Это не число!");
+    return;
+}
+
+// Определение числа по модулю
+int nnn=Math.Abs(Convert.ToInt32(str));
+
+// Вывод третьей цифры
+if (nnn.ToString().Length < 3) {
+    Console.WriteLine("Третьей цифры нет");
+}
+else {
+    Console.WriteLine("Третья цифра в " + nnn + " равна "+ nnn.ToString()[2]);
+}
