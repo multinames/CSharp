225fa9e0b761fffb7db65e7a378468d5c4b0a0f7
cepera <s.skuridin@corp.rtln.ru>
2023-05-24 12:18:50 +0300

    add news task10

---
+++ Task10/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Введите трехзначное число");
+
+string str = Console.ReadLine();
+
+// Проверка на число
+if ((int.TryParse(str, out int num)) == false) {
+  Console.WriteLine("Это не число!");
+    return;
+}
+
+// Определение числа по модулю
+int nnn=Math.Abs(Convert.ToInt32(str));
+
+// Проверка числа на разрядность   
+if (nnn.ToString().Length != 3) {
+    Console.WriteLine("Число не трехзначное");
+    return;
+}  
+
+// Вывод второй цифры
+Console.WriteLine("Вторая цифра в " + nnn + " равна "+ nnn.ToString()[1]);
+
+
