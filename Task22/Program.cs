53a92dc0a902480007b1e31f1008cea6e5a32a1c
Sergey Skuridin <skuridin@gmail.com>
2023-05-26 21:46:20 +0300

    add task 22

---
+++ Task22/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 22:
+
+int Prompt (string message) {
+Console.Write(message);
+int num = int.Parse(Console.ReadLine()!);
+return num;
+}
+
+void PrintDegreTable(int number)
+{
+    for (int i = 1; i <= number; i++)
+    {
+        Console.Write(Math.Pow(i, 2) + " ");
+    }
+}
+
+
+int num = Prompt ("Введите число N: ");
+
+// Определение числа по модулю
+int number = Math.Abs(num);
+
+
+PrintDegreTable(number);
+
+
+
+
