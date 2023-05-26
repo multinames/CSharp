847e5626cd688182a7251af1d0ab13d672e2eae3
Sergey Skuridin <skuridin@gmail.com>
2023-05-26 20:55:12 +0300

    add task 18

---
+++ Task18/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 17:
+
+int num = Prompt ("Введите номер четверти: ");
+
+int Prompt (string message) {
+Console.Write(message);
+int num = Convert.ToInt32(Console.ReadLine());
+return num;
+}
+
+void FindCoordinate (int num) {
+    if ( num == 1) {
+    Console.WriteLine("I четверть X > 0, Y > 0");
+    }
+    if ( num == 2) {
+    Console.WriteLine("II четверть X < 0 Y > 0");
+    }
+    if ( num == 3) {
+    Console.WriteLine("III четверть X < 0 Y < 0");
+    }
+    if ( num == 4) {
+    Console.WriteLine("IV четверть X > 0 Y < 0");
+    }
+    
+}
+
+if (num <1 || num >4) {
+
+    Console.WriteLine("Введена не правильная четверть");
+    }
+else {
+    FindCoordinate (num);
+}
+
+
+
