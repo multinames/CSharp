6f54ab7f4ff3dbce907aba941694e6c9e77f3991
cepera <s.skuridin@corp.rtln.ru>
2023-06-02 21:31:22 +0300

    add task29

---
+++ Task29/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+
+void ToAddInArray (int num_a) {
+    
+    int[] anum = new int[num_a];
+    Random randNum = new Random();
+
+    for(int i = 0; i < anum.Length; i++ ) {
+            anum[i]=randNum.Next(10);
+           }
+    Console.WriteLine(string.Join(" ",anum));
+
+}
+
+ToAddInArray(8);
