73c203d5ef1cd9a9097cbc7016e51ed3a69f1bc8
Sergey Skuridin <skuridin@gmail.com>
2023-06-16 20:22:39 +0300

    add Task 62

---
+++ Task63/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 63: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+
+int Prompt (string message) {
+Console.Write(message);
+string str = Console.ReadLine()!;
+    // Проверка вводимых данных на число
+    if ((int.TryParse(str, out int num)) == false) {
+    Console.WriteLine("Это не число!");   
+    }
+
+    return num;
+}
+
+void NaturalNumber(int number, int count) 
+{
+    if (count<=number) {
+    Console.WriteLine(count);
+    NaturalNumber(number, count+1);
+    }
+}
+
+int number = Prompt("Введите число: ");
+// int i = 1;
+
+NaturalNumber(number,1);
