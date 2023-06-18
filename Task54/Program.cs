0a97fb6a67d5263ccb525e47ff828cbd1e1c5cc1
Sergey Skuridin <skuridin@gmail.com>
2023-06-18 22:29:34 +0300

    add Task 54,61

---
+++ Task54/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Console.Clear();
+
+int[,] GetArray(int m, int n, int minValue, int maxValue) 
+{
+        int[,] result = new int[m,n];
+        
+    for (int i=0; i<m; i++) 
+    { 
+        for(int j=0; j<n; j++) 
+        {
+        result[i,j] = new Random().Next(minValue, maxValue);
+        }   
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray) {
+for(int i=0; i < inArray.GetLength(0); i++) // Отвечает за столбцы 
+    {
+    for(int j = 0; j < inArray.GetLength(1); j++) // Отвечает за строки
+        {
+            Console.Write($"{inArray[i, j]} ");
+
+        }
+    Console.WriteLine();
+    } 
+}
+
+void GetValueArray(int[,] inArray) 
+{
+  // Console.Write($"Среднее арифметическое каждого столбца: ");
+  
+  for(int i=0; i < inArray.GetLength(0); i++) 
+    
+    {
+    double sum =0;
+     for(int j = 0; j < inArray.GetLength(1); j++) 
+            {
+               sum+=inArray[i, j];
+            }
+    // double res = Math.Round( sum / inArray.GetLength(1), 2);
+         
+     Console.Write($"{sum} ");
+     }
+     
+}
+
+// задаем размерность массива
+int rows = 4;
+int columns = 4;
+
+// создание массива
+int[,] array = GetArray(rows, columns, 0, 10);
+
+
+PrintArray(array);
+GetValueArray(array);
---
+++ Task61/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Clear();
+
+Console.Write("Введите нужное количество строк треугольника Паскаля: ");
+int rows = int.Parse(Console.ReadLine());
+PrintTriangle(rows);
+
+
+void PrintTriangle(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int c = 0; c <= (n - i); c++) // создаём после каждой строки n-i отступов от левой стороны консоли, чем ниже строка, тем меньше отступ
+        {
+            Console.Write("  ");
+        }
+        for (int c = 0; c <= i; c++)
+        {
+            Console.Write("   "); // создаём пробелы между элементами треугольника
+            Console.Write(factorial(i) / (factorial(c) * factorial(i - c))); //формула вычисления элементов треугольника
+        }
+        Console.WriteLine(); // после каждой строки с числами отступаем две пустые строчки
+    }
+}
+float factorial(int n)
+{
+    float x = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        x *= i;
+    }
+    return x;
+}
+ 
