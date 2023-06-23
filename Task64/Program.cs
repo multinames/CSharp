4f1dacab60e7c2c4505600a4f62531cd83839361
Sergey Skuridin <skuridin@gmail.com>
2023-06-23 16:52:58 +0300

    add Task 64

---
+++ Task64/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+
+int Prompt (string message) {
+Console.Write(message);
+string str = Console.ReadLine()!;
+    // Проверка вводимых данных на число
+    if ((int.TryParse(str, out int num)) == false) {
+    Console.WriteLine("Это не число!");   
+    }
+
+    return num;
+}
+
+void NaturalNumber(int number, int count) 
+{
+    
+    if (count>=1) {
+    Console.Write($"{count} ");
+    NaturalNumber(number, count-1);
+    }
+}
+
+int number = Prompt("Введите число: ");
+
+NaturalNumber(number,number);
